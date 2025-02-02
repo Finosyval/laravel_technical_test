22463635a8d0c8f7f244631d55ed99a33657ac13
AMEGBEDJI Romuald Fervic Précieux <126726559+Essogbe@users.noreply.github.com>
2025-02-02 19:19:32 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 composer install --no-interaction --optimize-autoloader --ignore-platform-req=ext-dom --ignore-platform-req=ext-xml
 echo "Packages installed"
 php artisan key:generate --force
-php artisan db:seed --force
+#php artisan db:seed --force
 php artisan cache:clear
 php artisan migrate --force
